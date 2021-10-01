07cbf5a3fc1d873e600b03449f161fc31065f097
Rinzler007 <harshdasrox007@gmail.com>
2021-10-01 11:19:06 +0530

    Few changes in database

--- django-code-base/books/views.py
+++ django-code-base/books/views.py
@@ -4,22 +4,27 @@ from django.http import HttpResponse
 from .models import Book
 
 # Create your views here.
-@login_required(login_url='/login/')
 
-def collectionView( request ):
+
+@login_required(login_url='/login/')
+def collectionView(request):
     books = Book.objects.all()[:5]
     context = {
-       'books' : books
+        'books': books
     }
     print(context['books'])
+    print(f"The GET req : {request}")
     return render(request, 'books/collection.html', context)
 
-def readerView( request ):
-   # Error here have to check this part again.
-   book = Book.objects.get(bookName='Hello')
-   context = {
-      'bookPDF' : book,
-      'test' : "../../media/BookPDF/PCPG54.pdf"
-   }
-   print(context['bookPDF'].location.url)
-   return render(request, 'books/book.html', context)
+
+def readerView(request):
+    # Error here have to check this part again.
+    print(f"The GET req : {request.get_full_path()}")
+    book = Book.objects.get(bookName='HulaHulaHU')
+    context = {
+        'bookPDF': book,
+        'test': "../../media/BookPDF/PCPG54.pdf"
+    }
+    print(book)
+    print(context['bookPDF'].location.url)
+    return render(request, 'books/book.html', context)
